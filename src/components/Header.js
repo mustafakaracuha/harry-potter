8f893b1a7c8cad805d5f51fe1191e9b137e5bafc
mustafakaracuha <mustafa.karacuha@detaysoft.com>
2022-07-30 00:24:16 +0300

    edit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,32 +1,10 @@
 import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
 import { NavLink } from "react-router-dom";
-import { search, load } from "../stores/personajes";
-import { getPersonajes } from "../api/api";
 import logo from "../assets/images/Harry-Potter-Logo.png";
 import "../assets/scss/Home.scss";
 import "../assets/scss/Header.scss";
 
 export default function () {
-  const dispatch = useDispatch();
-
-  const searchCharacter = (event) => {
-    if (event.target.value) {
-      dispatch(search(event.target.value));
-    } else {
-      handleFetchData();
-    }
-  };
-
-  const handleFetchData = async () => {
-    const response = await getPersonajes()
-      .then((result) => {
-        dispatch(load(result.data));
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
 
   useEffect(() => {
   }, []);
@@ -46,7 +24,6 @@ export default function () {
           </li>
         </ul>
       </div>
-      {/* <input placeholder="Search character" onChange={searchCharacter} /> */}
     </div>
   );
 }
--- src/stores/personajes.js
+++ src/stores/personajes.js
@@ -11,16 +11,8 @@ const person = createSlice({
     load: (state, action) => {
       state.personajes = action.payload;
     },
-    search: (state, action) => {
-      if (action.payload) {
-        let filtered = state.personajes.filter((value) =>
-          value.personaje.toLowerCase().includes(action.payload.toLowerCase())
-        );
-        state.personajes = filtered;
-      }
-    },
   },
 });
 
-export const { load, search } = person.actions;
+export const { load } = person.actions;
 export default person.reducer;
